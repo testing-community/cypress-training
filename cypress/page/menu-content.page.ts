744da59bec470f576ccfde29a8513b575dcc0159
renardete <juanfe_518@hotmail.com>
2021-02-20 16:25:58 -0500

    fix pr

--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,9 +1,11 @@
 class MenuContentPage {
   private tShirtMenu: string;
+  private dressesMenu: string;
   private menuContentPageURL: string
 
   constructor() {
       this.tShirtMenu = '#block_top_menu > ul > li:nth-child(3) > a';
+      this.dressesMenu = '#block_top_menu > ul > li:nth-child(2) > a'
       this.menuContentPageURL = 'http://automationpractice.com/'
   }
 
@@ -13,6 +15,10 @@ class MenuContentPage {
 
   public goToTShirtMenu(): void {
       cy.get(this.tShirtMenu).click()
+  }
+
+  public goToDressesMenu(): void {
+    cy.get(this.dressesMenu).click()
   }
 }
 
