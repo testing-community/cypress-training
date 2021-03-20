76a622f9d6801fc8cdac4282ae8e894fa9f8a4c6
Alejandro Perdomo <alejandro.perdomob@perficient.com>
2021-03-20 12:01:16 -0500

    refactor: update project with last recommendations

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,15 +11,12 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const wp = require('@cypress/webpack-preprocessor')
 
 /**
  * @type {Cypress.PluginConfig}
  */
+// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  const options = {
-    webpackOptions: require('../../webpack.config'),
-  }
-
-  on('file:preprocessor', wp(options))
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
 }
